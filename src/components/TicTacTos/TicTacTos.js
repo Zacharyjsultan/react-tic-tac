13fcdb78875ef72afeb74f02aea3280622cb9edd
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-07 16:49:13 -0700

    as

--- src/components/TicTacTos/TicTacTos.js
+++ src/components/TicTacTos/TicTacTos.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../../context/GameContext';
-import 
+
 export default function TicTacTos({ box, mark }) {
   const { moveMade } = useContext(GameContext);
   return (
