7ad369f4856ee747d4f6b53ed00d9d068e12ae53
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-10-13 12:21:44 -0700

    working on context logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react';
 import { GameContext } from '../../context/GameContext';
 
 export default function Header() {
-  const { message } = useContext(GameContext);
+  const { message, newGame, active } = useContext(GameContext);
   return (
     <>
       <h2 className="message">{message}</h2>
+      {!active && <button onClick={newGame}> Play Again</button>}
     </>
   );
 }
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,16 +3,34 @@ import { createContext, useState } from 'react';
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const initialState = new Array(9).fill().map((val, idx) => ({ space: idx, content: '' }));
+  const initialState = new Array(9).fill().map((val, idx) => ({ box: idx, mark: '' }));
 
   const [board, setBoard] = useState(initialState);
   const [activePlayer, setActivePlayer] = useState('O');
   const [message, setMessage] = useState(`Hurry up, ${activePlayer}...`);
   const [active, setActive] = useState(true);
 
+  const newGame = () => {
+    setBoard(initialState);
+    setActive(true);
+    setMessage(`Hurry up ${activePlayer}`);
+  };
+
+  const updateMark = (num) => {
+    if (!active) return;
+    if (board[num].mark !== '') return;
+
+    setBoard((prev) =>
+      prev.map((space) => (space.box === num ? { box: num, mark: active } : space))
+    );
+    setActivePlayer(activePlayer === 'X' ? 'O' : 'X');
+  };
+
   return (
     <GameContext.Provider
       value={{
+        newGame,
+        updateMark,
         board,
         setBoard,
         activePlayer,
